82303604864dfbf4720eab1c134708797e2eee7d
Himanshi Aggarwal <96831313+himanshi2003@users.noreply.github.com>
2023-03-13 11:55:04 +0530

    Add files via upload

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int sum=0;
+       for(int i=0;i<nums.size();i++){
+           sum=sum+nums[i];
+       }
+       int actualsum=(nums.size()*(nums.size()+1))/2;
+       int missingnum=actualsum-sum;
+
+       return missingnum;
+    }
+};
