db4e35071d5c5cf5bba20ce3a9d5d059a8d512c8
Himanshi Aggarwal <96831313+himanshi2003@users.noreply.github.com>
2023-03-23 12:55:45 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        nums.size();
+        
         sort(nums.begin(), nums.end());
 
         return nums[nums.size()-k];
